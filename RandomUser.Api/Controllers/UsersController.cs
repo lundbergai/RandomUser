52ecb000f53325f826f62136fcc7a1e4581f01af
Viktor Lundberg <vikkolundberg@gmail.com>
2025-10-20 13:19:18 +0200

    add userscontroller with get users method

---
+++ RandomUser.Api/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RandomUser.Domain.Entities;
+using RandomUser.Infrastructure;
+
+namespace RandomUser.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly RandomUserDbContext _context;
+
+        public UsersController(RandomUserDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: api/users
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return await _context.Users
+                .Include(u => u.Name)
+                .Include(u => u.Location)
+                .ThenInclude(l => l.Street)
+                .Include(u => u.Location)
+                .ThenInclude(l => l.Coordinates)
+                .Include(u => u.Location)
+                .ThenInclude(l => l.TimeZone)
+                .ToListAsync();
+        }
+        
+    }
+}
