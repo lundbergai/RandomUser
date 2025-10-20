fae87883a299771dc5b6503e639577da64ffb788
Viktor Lundberg <vikkolundberg@gmail.com>
2025-10-20 20:51:16 +0200

    create database commands to clear and fetch users

---
+++ RandomUser.Application/Commands/ClearDbCommand.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RandomUser.Infrastructure.Persistence;
+
+namespace RandomUser.Application.Commands;
+
+public class ClearDbCommand : IClearDbCommand
+{
+    private readonly IRandomUserDbContext _dbContext;
+    
+    public ClearDbCommand(IRandomUserDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> ExecuteAsync()
+    {
+        var users = await _dbContext.Users.ToListAsync();
+        _dbContext.Users.RemoveRange(users);
+        
+        return await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ RandomUser.Application/Commands/FetchSaveUsersCommand.cs
@@ -0,0 +1,25 @@
+﻿using RandomUser.Infrastructure.Persistence;
+using RandomUser.Infrastructure.Services;
+
+namespace RandomUser.Application.Commands;
+
+public class FetchSaveUsersCommand : IFetchSaveUsersCommand
+{
+    private readonly IRandomUserApiService _apiService;
+    private readonly IRandomUserDbContext _dbContext;
+
+    public FetchSaveUsersCommand(IRandomUserApiService apiService, IRandomUserDbContext dbContext)
+    {
+        _apiService = apiService;
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> ExecuteAsync(int count)
+    {
+        var users = await _apiService.FetchUsersAsync(count);
+        
+        await _dbContext.Users.AddRangeAsync(users); 
+        
+        return await _dbContext.SaveChangesAsync();
+    }
+}
