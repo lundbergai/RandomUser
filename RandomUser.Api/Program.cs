0b8ea142b6f1f3815ec73c76b42253d62de2bf39
Viktor Lundberg <vikkolundberg@gmail.com>
2025-10-20 20:51:58 +0200

    register services

--- RandomUser.Api/Program.cs
+++ RandomUser.Api/Program.cs
@@ -1,21 +1,33 @@
 using Microsoft.EntityFrameworkCore;
+using RandomUser.Application.Commands;
 using RandomUser.Application.Queries.Countries;
 using RandomUser.Infrastructure.Persistence.Seed;
 using RandomUser.Infrastructure.Persistence;
 using RandomUser.Infrastructure;
+using RandomUser.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
 
+// Context
 builder.Services.AddDbContext<RandomUserDbContext>(options =>
     options.UseSqlite("Data Source=randomuser.db"));
 
 builder.Services.AddScoped<IRandomUserDbContext>(provider =>
     provider.GetRequiredService<RandomUserDbContext>());
 
+// Commands
+builder.Services.AddScoped<IFetchSaveUsersCommand, FetchSaveUsersCommand>();
+builder.Services.AddScoped<IClearDbCommand, ClearDbCommand>();
+
+// Queries
 builder.Services.AddScoped<IGetCountriesQuery, GetCountriesQuery>();
+
+// Other Services
+builder.Services.AddScoped<IRandomUserApiService, RandomUserApiService>();
+builder.Services.AddHttpClient<IRandomUserApiService, RandomUserApiService>();
 
 // Add Swagger
 builder.Services.AddEndpointsApiExplorer();
