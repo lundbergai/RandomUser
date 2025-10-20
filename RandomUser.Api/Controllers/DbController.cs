31dd9d4201f7c0293c46e433f50a22d6b27ecba1
Viktor Lundberg <vikkolundberg@gmail.com>
2025-10-20 20:51:39 +0200

    create database controller for fetching and clearing db

---
+++ RandomUser.Api/Controllers/DbController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomUser.Application.Commands;
+
+namespace RandomUser.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DbController : ControllerBase
+{
+    private readonly IFetchSaveUsersCommand _fetchSaveUsers;
+    private readonly IClearDbCommand _clearDb;
+
+    public DbController(IFetchSaveUsersCommand fetchSaveUsers, IClearDbCommand clearDb)
+    {
+        _fetchSaveUsers = fetchSaveUsers;
+        _clearDb = clearDb;
+    }
+
+    [HttpPost("fetch")]
+    public async Task<ActionResult> FetchUsers([FromQuery] int count = 50)
+    {
+        var addedCount = await _fetchSaveUsers.ExecuteAsync(count);
+        
+        return Ok(new { message = $"{addedCount} rows affected" });
+    }
+    
+    [HttpPost("clear")]
+    public async Task<ActionResult> ClearDatabase()
+    {
+        var deletedCount = await _clearDb.ExecuteAsync();
+        
+        return Ok(new { message = $"{deletedCount} rows affected" });
+    }
+}
