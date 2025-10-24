22b8195016a25759545d7103c09015b345f0b4e4
Viktor Lundberg <vikkolundberg@gmail.com>
2025-10-24 15:04:55 +0200

    move seed

--- RandomUser.Infrastructure/Seed/Seed.cs
+++ RandomUser.Infrastructure/Seed/Seed.cs
@@ -2,8 +2,9 @@ using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using RandomUser.Domain.Entities;
 using System.Reflection;
+using RandomUser.Infrastructure.Persistence;
 
-namespace RandomUser.Infrastructure.Persistence.Seed;
+namespace RandomUser.Infrastructure.Seed;
 
 public class Seed
 {
@@ -14,21 +15,21 @@ public class Seed
             // https://randomuser.me/api/?results=50&inc=name,location,phone
             var assemblyLocation = Assembly.GetExecutingAssembly().Location;
             var assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
-            var jsonFilePath = Path.Combine(assemblyDirectory, "Persistence", "Seed", "randomUser.json");
-            
+            var jsonFilePath = Path.Combine(assemblyDirectory, "Seed", "randomUser.json");
+
             var json = await File.ReadAllTextAsync(jsonFilePath);
-            
+
             var options = new JsonSerializerOptions
             {
                 Converters = { new StringConverter() },
                 PropertyNameCaseInsensitive = true
             };
-            
+
             var data = JsonSerializer.Deserialize<UserResponse>(json, options);
 
             if (data?.Results != null)
             {
-                await dbContext.Users.AddRangeAsync(data.Results); 
+                await dbContext.Users.AddRangeAsync(data.Results);
                 await dbContext.SaveChangesAsync();
             }
         }
